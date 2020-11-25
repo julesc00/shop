f439a65774dd997b44b54e8a3f501675bb855ed3
juliobriones <julesc00@hotmail.com>
2020-11-25 07:53:32 -0600

    Added custom fonts.

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 import { createStore, combineReducers } from "redux"
 import { Provider } from "react-redux"
+import { AppLoading } from "expo"
+import * as Font from "expo-font"
 
 import productsReducer from "./store/reducers/products"
+import ShopNavigator from "./navigation/ShopNavigator";
 
 const rootReducer = combineReducers({
   products: productsReducer
@@ -12,21 +14,23 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer)
 
+const fetchFont = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
+  })
+}
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  if (!fontLoaded) {
+    return <AppLoading startAsync={fetchFont} onFinish={() => setFontLoaded(true)} />
+  }
+
   return (
     <Provider store={store}>
-      <View>
-        <Text>Chill!</Text>
-      </View>
+      <ShopNavigator />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/shop/ProductOverviewScreen.js
+++ screens/shop/ProductOverviewScreen.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { View, Text, FlatList, StyleSheet } from "react-native";
+import { useSelector } from "react-redux"
+
+import ProductItem from "../../components/shop/ProductItem"
 
 const ProductOverviewScreen = props => {
+  const products = useSelector(state => state.products.availableProducts)
+
   return (
-    <View>
-      <FlatList />
-    </View>
+      <FlatList
+        data={products}
+        keyExtractor={item => item.id}
+        renderItem={itemData => (
+          <ProductItem
+            image={itemData.item.imageUrl}
+            title={itemData.item.title}
+            price={itemData.item.price}
+            onViewDetail={() => {
+              props.navigation.navigate("ProductDetail", {
+                productId: itemData.item.id,
+                productTitle: itemData.item.title
+              })
+            }}
+            onAddToCart={() => {}}
+          />
+        )}
+      />
   )
+}
+
+ProductOverviewScreen.navigationOptions = {
+  headerTitle: "All Products"
 }
 
 const styles = StyleSheet.create({
